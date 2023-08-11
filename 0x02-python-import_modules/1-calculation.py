5082d413195add112f31291db30b6cbf97587466
simonmuzariri <simonsammuzariri@gmail.com>
2023-08-11 01:47:29 -0700

    edited

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1, import add, sub, mul, div
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
-    a = 10
-    b = 5
+    x = 10
+    z = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(x, z, add(x, z)))
+    print("{} - {} = {}".format(x, z, sub(x, z)))
+    print("{} * {} = {}".format(x, z, mul(x, z)))
+    print("{} / {} = {}".format(x, z, div(x, z)))
+
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
 
     count = len(sys.argv) - 1
@@ -9,5 +11,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i +1]))
+    for a in range(count):
+        print("{}: {}".format(i + 1, sys.argv[a + 1]))
